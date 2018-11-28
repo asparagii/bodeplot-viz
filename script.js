eec081e811f665738c1398fc0186713a552c3b39
Michele Lambertucci <michele.lambertucci1@gmail.com>
2018-11-28 19:06:54 +0100

    Added phase. Fixed scaling a little bit

--- script.js
+++ script.js
@@ -23,66 +23,87 @@ function _ready(){
 
     input_textbox = document.getElementById("transfer-function")
     input_textbox.addEventListener("change", update_tf, false)
-    input_button = document.getElementById("button-compute")
-    input_button.disabled = true
-    input_button.addEventListener("click", bode_plot, false)
 }
 
 let tf = math.parse('s')
 let compiled
 
 function update_tf(){
-    input_button.disabled = true
     tf = math.parse(input_textbox.value)
     compiled = tf.compile()
     try {
         compiled.eval({'s': math.complex(0, 1)})
-        input_button.disabled = false
         document.getElementById("output").textContent = tf
+        bode_plot()
     } catch(err){
         document.getElementById("output").textContent = err
     }
 }
 
+let modulePlot
+let phasePlot
+
 function bode_plot(){
-    var y = xs.map((w) => {
-        return compiled.eval({s: math.complex(0, w)}).abs()
+    ys = xs.map((w) => {
+        return compiled.eval({s: math.complex(0, w)}).toPolar()
     })
 
-    var ctx = document.getElementById("plot").getContext('2d')
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
+    var y_module = ys.map(c => c.r)
+    var y_phase = ys.map(c => c.phi / math.pi * 180)
+
+    function create_data(y, label){
+        return {
             labels: xs,
             datasets: [{
+                label: label,
+                fill: false,
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
                 data: y
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    type: 'logarithmic'
-                }]
-            }
         }
-        // ,
-        // options: {
-        //     scales: {
-        //         xAxes: [{
-        //             type: 'linear',
-        //             ticks: {
-        //                 min: 0.01,
-        //                 max: 1e6
-        //             }
-        //         }],
-        //         yAxes: [{
-        //             type: 'linear',
-        //             ticks: {
-        //                 min: 0.01,
-        //                 max: 1e2
-        //             }
-        //         }]
-        //     }
-        // }
-    })
+    }
+
+    if(modulePlot == undefined){
+        var modulePlotContext = document.getElementById("module-plot").getContext('2d')
+        modulePlot = new Chart(modulePlotContext, {
+            type: 'line',
+            data: create_data(y_module, "|T(ωj)|"),
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,        
+                scales: {
+                    yAxes: [{
+                        type: 'logarithmic'
+                    }]
+                }
+            }
+        })
+    } else {
+        modulePlot.config.data = create_data(y_module, "|T(ωj)|")
+        modulePlot.update();
+    }
+    
+    if(phasePlot == undefined){
+        var phasePlotContext = document.getElementById("phase-plot").getContext('2d')
+        phasePlot = new Chart(phasePlotContext, {
+            type: 'line',
+            data: create_data(y_phase, "∠T(ωj)"),
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,        
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            suggestedMin: -90,
+                            suggestedMax: 90
+                        }
+                    }]
+                }
+            }
+        })
+    } else {
+        phasePlot.config.data = create_data(y_phase, "∠T(ωj)")
+        phasePlot.update();
+    }
 }
